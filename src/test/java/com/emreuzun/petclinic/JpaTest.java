67317693f04d2439e571e51bf24904e6721ba65d
uzunemre <emreuzun92@gmail.com>
2019-07-19 00:12:29 +0300

    Jpa Transaction

--- src/test/java/com/emreuzun/petclinic/JpaTest.java
+++ src/test/java/com/emreuzun/petclinic/JpaTest.java
@@ -1,6 +1,7 @@
 package com.emreuzun.petclinic;
 
 import com.emreuzun.petclinic.config.JpaConfig;
+import com.emreuzun.petclinic.model.Pet;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
@@ -9,13 +10,29 @@ import javax.persistence.EntityTransaction;
 public class JpaTest {
 
     @Test
-    public void testJpaSetup(){
+    public void testJpaSetup() {
         EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
         tx.commit();
         entityManager.close();
         JpaConfig.getEntityManagerFactory().close();
+    }
+
+    @Test
+    public void testWithoutTX() {
+        EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        Pet pet = new Pet();
+        pet.setId(1L);
+        pet.setName("kedicik");
+
+        entityManager.persist(pet);
+        //entityManager.flush();
+        tx.commit();
+        entityManager.close();
     }
 
 }
