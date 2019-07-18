a169f75f776c413d8ca354de15ebecb10a9d0c88
uzunemre <emreuzun92@gmail.com>
2019-07-18 23:47:01 +0300

    Entity yazılması Basic Property ve Assigned Pk kullanımı

--- src/test/java/com/emreuzun/petclinic/HibernateTest.java
+++ src/test/java/com/emreuzun/petclinic/HibernateTest.java
@@ -1,6 +1,7 @@
 package com.emreuzun.petclinic;
 
 import com.emreuzun.petclinic.config.HibernateConfig;
+import com.emreuzun.petclinic.model.Pet;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
@@ -16,6 +17,20 @@ public class HibernateTest {
         HibernateConfig.getSessionFactory().close();
     }
 
+    @Test
+    public void testCreateEntity() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = new Pet();
+        pet.setId(1L);
+        pet.setName("kedicik");
+
+        session.persist(pet);
+
+        tx.commit();
+        session.close();
+    }
 
 
 }
