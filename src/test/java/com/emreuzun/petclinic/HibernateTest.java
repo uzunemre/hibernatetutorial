71040951c623065f5bc7ccfc7200b1e40f821bea
uzunemre <uzunemre92@gmail.com>
2019-09-09 00:11:36 +0300

    persistence işlemleri

--- src/test/java/com/emreuzun/petclinic/HibernateTest.java
+++ src/test/java/com/emreuzun/petclinic/HibernateTest.java
@@ -1,22 +1,264 @@
 package com.emreuzun.petclinic;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Map;
 
+import com.emreuzun.petclinic.model.*;
+import org.hibernate.Hibernate;
+import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.stat.EntityStatistics;
+import org.hibernate.stat.QueryStatistics;
+import org.hibernate.stat.Statistics;
 import org.junit.Test;
 import com.emreuzun.petclinic.config.HibernateConfig;
-import com.emreuzun.petclinic.model.Address;
-import com.emreuzun.petclinic.model.Image;
-import com.emreuzun.petclinic.model.Owner;
-import com.emreuzun.petclinic.model.OwnerWithCompositePK;
 import com.emreuzun.petclinic.model.OwnerWithCompositePK.OwnerId;
-import com.emreuzun.petclinic.model.Pet;
-import com.emreuzun.petclinic.model.Rating;
-import com.emreuzun.petclinic.model.Visit;
 
 public class HibernateTest {
 
+    /**
+     * refresh methodu veritabanınan gider ve veriyi çeker.(first level cache'e bakmaz)
+     * @throws InterruptedException
+     */
+    @Test
+    public void testRefresh() throws  InterruptedException {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        PetType pt = session.get(PetType.class, 1L);
+        System.out.println("--- PetType loaded ---");
+        pt.setName("xxx");
+
+        System.out.println("--- waiting... ---");
+
+        Thread.sleep(10000);
+
+        session.refresh(pt);
+
+        System.out.println("--- after refresh ---");
+
+        System.out.println(pt.getName());
+    }
+
+    /**
+     * veritabanında herhangi bir değişiklik olmaz. commit gerekli
+     */
+    @Test
+    public void testFlushTxRelationship() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Owner owner = session.get(Owner.class, 7L);
+        owner.setRating(null);
+
+        session.persist(new Pet("my pet", new Date()));
+
+        session.flush();
+        System.out.println("--- after flush ---");
+    }
+
+    @Test
+    public void testDelete() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Visit visit = session.get(Visit.class, 2L);
+
+        session.clear(); // detached olsa bile silme işlemi gerçekleşir
+
+
+
+        session.delete(visit);
+
+        tx.commit();
+        session.close();
+    }
+
+
+    /**
+     * hibernate.enable_lazy_load_no_trans true set edilince session kapalı olsa bile kendisi session açar ve hata vermez
+     */
+    @Test
+    public void testHibernateInitialize() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = session.load(Pet.class, 1L);
+
+        System.out.println("--- before initialize ---");
+
+        //Hibernate.initialize(pet);
+        //Hibernate.initialize(pet.getImagesByFilePath());
+
+        session.close();
+
+        System.out.println("--- session closed ---");
+
+        System.out.println(pet.getName());
+        System.out.println(pet.getImagesByFilePath().size());
+    }
+
+    /**
+     * pet nesnesi detached yapıldıktan sonra  pet.getImagesByFilePath()  çağırılıdğında  LazyInitializationException alırız.
+     * imagesByFilePath nesnesi proxy dir. pet nesnesi detached olduğu için initialize edilemiyor
+     * session.update(pet) çalıştırıldığında nesne tekrar attached olur hata vermez
+     */
+    @Test
+    public void testDetachedEntitiesAndLazy() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = session.get(Pet.class, 1L);
+
+        //session.close();
+        //session.clear();
+        session.evict(pet);
+
+        System.out.println("--- after session evict ---");
+
+        System.out.println("session open :" + session.isOpen());
+
+        //session.update(pet);
+        //session.lock(pet, LockMode.NONE); reaatach yapar. ismi yanıltıcı. updateden farkı değişikliği locklarken yapmaz. parametre olarak verilen pet nesnesi
+        // snapshota eklenir lockdan sonra değişiklik varsa commit aşamasında veritabanına yansır
+
+        session.merge(pet);
+
+        System.out.println(pet.getClass());
+
+        Map<String, Image> imagesByFilePath = pet.getImagesByFilePath();
+        System.out.println(imagesByFilePath.getClass());
+        System.out.println("image size :" + imagesByFilePath.size());
+
+    }
+
+
+    @Test
+    public void testDetachedEntities() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        // pet nesnesi attached ediliyor
+        Pet pet = session.get(Pet.class, 1L);
+
+        tx.commit();
+        session.close();
+        // pet nesnesi detached ediliyor
+
+
+        session = HibernateConfig.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+
+        // pet nesnesi detached olduğu için yapılan değişikler veritabanınan yansıtılmayacaktır. reattach yapılırsa değişiklik yansır
+        pet.setBirthDate(new Date());
+
+        tx.commit();
+        session.close();
+    }
+
+    @Test
+    public void testUpdate() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = session.get(Pet.class, 1L);
+
+        pet.setBirthDate(null);
+        pet.setType(session.load(PetType.class, 3L));
+
+        // pet nesesinde işlem yapıldığı için commit aşamasında pet için update sorguları çalıştırılır
+        tx.commit();
+        session.close();
+    }
+
+    /**
+     * yeni bir entity referansı döner. pet nesnesinde işlem yapıldıktan sonra bir daha işlem yapılmaması gerekiyor.
+     * pet nesnesinde id değeri set edilmez merge kullanıldığı için
+     */
+    @Test
+    public void testInsertWithMerge() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = new Pet();
+        pet.setName("kedicik 3");
+
+        Pet pet2 = (Pet) session.merge(pet);
+
+        System.out.println("--- after merge ---");
+
+        pet.setBirthDate(new Date());
+
+        System.out.println("pet id :" + pet.getId());
+        System.out.println("pet 2 id :" + pet2.getId());
+        System.out.println(pet == pet2);
+        System.out.println("pet 2 birth date :" + pet2.getBirthDate());
+
+        tx.commit();
+        session.close();
+    }
+
+
+    @Test
+    public void testSave() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = new Pet();
+        pet.setName("kedicik 2");
+
+        Serializable pk = session.save(pet); // save methodu oluşturulan pk döndürür
+
+        System.out.println("--- after save called ---");
+
+        tx.commit();
+        session.close();
+
+        System.out.println(pk == pet.getId());
+    }
+
+    @Test
+    public void testPersist() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Pet pet = new Pet();
+        pet.setName("kedicik 1");
+
+        session.persist(pet);
+        // persist yapılınca id değeri set edilir
+        System.out.println("--- after persist called ---");
+        tx.commit();
+        session.close();
+    }
+
+    @Test
+    public void testStatistics() {
+        Session session = HibernateConfig.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.persist(new Pet("kedicik", new Date()));
+
+        session.get(Pet.class, 1L);
+
+        session.flush();
+
+        session.createQuery("select p.name from Pet p").getResultList();
+
+        Statistics statistics = HibernateConfig.getSessionFactory().getStatistics();
+
+        EntityStatistics entityStatistics = statistics.getEntityStatistics("com.emreuzun.petclinic.model.Pet");
+        QueryStatistics queryStatistics = statistics.getQueryStatistics("select p.name from Pet p");
+
+        System.out.println("load count :" + entityStatistics.getLoadCount());
+        System.out.println("insert count :" + entityStatistics.getInsertCount());
+
+        System.out.println("query exec count :" + queryStatistics.getExecutionCount());
+        System.out.println("query avg exec time :" + queryStatistics.getExecutionAvgTime());
+    }
 
     @Test
     public void testLoad() {
@@ -27,7 +269,7 @@ public class HibernateTest {
         // pet kaydı için veritabanına bir select atmadı. bu nesne bir proxy nesne
         // proxy nesnede sadece primary key değer ivar. proxy nesneden name alanının çektiğimiz an select atar
         System.out.println("--- pet loaded ---");
-        if(pet == null) {
+        if (pet == null) {
             System.out.println("pet is null returning");
             return;
         }
@@ -51,7 +293,7 @@ public class HibernateTest {
 
         Pet pet = session.get(Pet.class, 1L);
         System.out.println("--- pet loaded ---");
-        if(pet == null) {
+        if (pet == null) {
             System.out.println("pet is null returning");
             return;
         }
@@ -242,7 +484,6 @@ public class HibernateTest {
 
         tx.commit();
         session.close();
-
 
 
     }
--- src/test/java/com/emreuzun/petclinic/JpaTest.java
+++ src/test/java/com/emreuzun/petclinic/JpaTest.java
@@ -4,12 +4,91 @@ import com.emreuzun.petclinic.config.JpaConfig;
 import com.emreuzun.petclinic.model.Owner;
 import com.emreuzun.petclinic.model.Pet;
 import com.emreuzun.petclinic.model.Rating;
+import com.emreuzun.petclinic.model.Visit;
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.stat.Statistics;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 public class JpaTest {
+
+    @Test
+    public void testDelete() {
+        EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        Visit visit = entityManager.find(Visit.class, 3L);
+
+        entityManager.clear();
+
+        visit = entityManager.merge(visit);
+
+        entityManager.remove(visit);
+
+        tx.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void testHibernateApiAccess2() {
+        EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        Session session = (Session) entityManager.getDelegate();
+        Session session2 = entityManager.unwrap(Session.class);
+
+        System.out.println(session == session2);
+
+        SessionFactory sf = session.getSessionFactory();
+
+        Statistics statistics = sf.getStatistics();
+        session.setHibernateFlushMode(FlushMode.MANUAL);
+    }
+
+    /**
+     * jpa ile çalışırken hibernate apisine erişim
+     */
+    @Test
+    public void testHibernateApiAccess() {
+        EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        SessionFactory sf = (SessionFactory)JpaConfig.getEntityManagerFactory();
+        Session s = (Session) entityManager;
+        Transaction hibTx = (Transaction)tx;
+
+        Statistics statistics = sf.getStatistics();
+        s.setHibernateFlushMode(FlushMode.MANUAL);
+    }
+
+    @Test
+    public void testFindAndGetReference() {
+        EntityManager entityManager = JpaConfig.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        Pet pet = entityManager.find(Pet.class, 1L);
+
+        System.out.println("--- pet loaded ---");
+
+        System.out.println(pet.getName());
+        System.out.println(pet.getClass());
+
+        Pet pet2 = entityManager.getReference(Pet.class, 2L); // proxy nesne
+
+        System.out.println("--- pet 2 loaded ---");
+
+        System.out.println(pet2.getName());
+        System.out.println(pet2.getClass());
+    }
 
     @Test
     public void testJpaSetup() {
